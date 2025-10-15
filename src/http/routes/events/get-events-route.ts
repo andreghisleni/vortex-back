06a6df9de9a8b11ddad7a7a9f183bed7f55d674a
André Ghisleni Raimann <andre@andreg.com.br>
2025-10-15 17:16:59 -0300

    Changes to be committed: 	modified:   src/http/routes/events/get-events-route.ts 	modified:   src/http/routes/events/schemas.ts

--- src/http/routes/events/get-events-route.ts
+++ src/http/routes/events/get-events-route.ts
@@ -1,31 +1,29 @@
-import Elysia, { t } from 'elysia';
-import { authMacro } from '~/auth';
-import { prisma } from '~/db/client';
-import { eventSchema } from './schemas';
+import Elysia, { t } from "elysia";
+import { authMacro } from "~/auth";
+import { prisma } from "~/db/client";
+import { eventSchema } from "./schemas";
 
-export const getEventsRoute = new Elysia()
-  .macro(authMacro)
-  .get(
-    '/',
-    async () => {
-      const e = await prisma.event.findMany({
-        orderBy: { createdAt: 'desc' },
-        include: {
-          ticketRanges: true
-        }
-      });
+export const getEventsRoute = new Elysia().macro(authMacro).get(
+  "/",
+  async () => {
+    const e = await prisma.event.findMany({
+      orderBy: { createdAt: "desc" },
+      include: {
+        ticketRanges: true,
+      },
+    });
 
-      return e;
+    return e;
+  },
+  {
+    auth: true,
+    detail: {
+      tags: ["Events"],
+      summary: "Get all events",
+      operationId: "getAllEvents",
     },
-    {
-      auth: true,
-      detail: {
-        tags: ['Events'],
-        summary: 'Get all events',
-        operationId: 'getAllEvents',
-      },
-      response: {
-        200: t.Array(eventSchema),
-      },
-    }
-  );
+    response: {
+      200: t.Array(eventSchema),
+    },
+  }
+);
--- src/http/routes/events/schemas.ts
+++ src/http/routes/events/schemas.ts
@@ -1,49 +1,51 @@
-import { t } from 'elysia';
+import { t } from "elysia";
 
 export const ticketTypeSchema = t.Union(
-  [t.Literal('SINGLE_NUMERATION'), t.Literal('MULTIPLE_NUMERATIONS')],
+  [t.Literal("SINGLE_NUMERATION"), t.Literal("MULTIPLE_NUMERATIONS")],
   {
-    default: 'SINGLE_NUMERATION',
-    description: 'Type of ticketing system used for the event',
+    default: "SINGLE_NUMERATION",
+    description: "Type of ticketing system used for the event",
   }
 );
 
 export const eventSchema = t.Object({
   id: t.String({
-    format: 'uuid',
-    description: 'Unique identifier for the event',
+    format: "uuid",
+    description: "Unique identifier for the event",
   }),
   name: t.String({
-    description: 'Name of the event',
+    description: "Name of the event",
     minLength: 3,
   }),
   description: t.Nullable(
     t.String({
-      description: 'Description of the event',
+      description: "Description of the event",
       maxLength: 500,
     })
   ),
   ticketType: ticketTypeSchema,
   ownerId: t.Nullable(
     t.String({
-      format: 'uuid',
-      description: 'Unique identifier for the owner of the event',
+      format: "uuid",
+      description: "Unique identifier for the owner of the event",
     })
   ),
   autoGenerateTicketsTotalPerMember: t.Nullable(t.Number()),
   readOnly: t.Boolean({ default: false }),
   createdAt: t.Date({
-    description: 'Timestamp when the event was created',
+    description: "Timestamp when the event was created",
   }),
   updatedAt: t.Date({
-    description: 'Timestamp when the event was last updated',
+    description: "Timestamp when the event was last updated",
   }),
   ticketRanges: t.Array(
-    t.Object({
-      id: t.String({ format: 'uuid' }),
-      start: t.Number(),
-      end: t.Number(),
-      type: t.String(),
-    })
+    t.Nullable(
+      t.Object({
+        id: t.String({ format: "uuid" }),
+        start: t.Number(),
+        end: t.Number(),
+        type: t.String(),
+      })
+    )
   ),
 });
