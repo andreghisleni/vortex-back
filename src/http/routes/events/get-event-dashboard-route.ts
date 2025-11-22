33de830c893d7f2b0d616cf96659671ddccbebe3
André Ghisleni Raimann <andre@andreg.com.br>
2025-11-22 19:12:50 -0300

    Changes to be committed: 	modified:   src/http/routes/events/get-event-dashboard-route.ts

--- src/http/routes/events/get-event-dashboard-route.ts
+++ src/http/routes/events/get-event-dashboard-route.ts
@@ -26,14 +26,16 @@ export const getEventDashboardRoute = new Elysia()
         totalTicketsAfterImport,
         totalWithCritica,
         totalWithCriticaAndDelivered,
-        totalWithoutCriticaCalabresa,
-        totalWithoutCriticaMista,
+        // totalWithoutCriticaCalabresa,
+        // totalWithoutCriticaMista,
         totalValuePayedTickets,
         totalValuePayedTicketsOnLastWeek,
-        membersWithPizzaAndPaymentData,
+        // membersWithPizzaAndPaymentData,
         totalMembers,
         ticketEventRanges,
         ticketEventRangesLinkedToMembers,
+        totalWithoutCriticaPerTicketEventRanges,
+        totalWithCriticaPerTicketEventRanges,
       ] = await prisma.$transaction([
         prisma.ticket.count({ where: { eventId: params.id } }),
         prisma.ticket.count({ where: { eventId: params.id, memberId: { not: null } } }),
@@ -42,11 +44,11 @@ export const getEventDashboardRoute = new Elysia()
         prisma.ticket.count({ where: { eventId: params.id, created: 'AFTERIMPORT', memberId: { not: null } } }),
         prisma.ticket.count({ where: { eventId: params.id, returned: true, memberId: { not: null } } }),
         prisma.ticket.count({ where: { eventId: params.id, returned: true, deliveredAt: { not: null }, memberId: { not: null } } }),
-        prisma.ticket.count({ where: { eventId: params.id, returned: false, number: { gte: 0, lte: 1000 }, memberId: { not: null } } }),
-        prisma.ticket.count({ where: { eventId: params.id, returned: false, number: { gte: 2000, lte: 3000 }, memberId: { not: null } } }),
+        // prisma.ticket.count({ where: { eventId: params.id, returned: false, number: { gte: 0, lte: 1000 }, memberId: { not: null } } }),
+        // prisma.ticket.count({ where: { eventId: params.id, returned: false, number: { gte: 2000, lte: 3000 }, memberId: { not: null } } }),
         prisma.payment.findMany({ where: { member: { eventId: params.id }, deletedAt: null } }),
         prisma.payment.findMany({ where: { member: { eventId: params.id }, deletedAt: null, payedAt: { gte: new Date(new Date().setDate(new Date().getDate() - 7)), lte: new Date() } } }),
-        prisma.member.findMany({ where: { eventId: params.id, tickets: { some: { returned: false } } }, include: { tickets: { select: { number: true }, where: { returned: false } }, payments: { select: { amount: true }, where: { deletedAt: null } } } }),
+        // prisma.member.findMany({ where: { eventId: params.id, tickets: { some: { returned: false } } }, include: { tickets: { select: { number: true }, where: { returned: false } }, payments: { select: { amount: true }, where: { deletedAt: null } } } }),
         prisma.member.count({ where: { eventId: params.id } }),
         prisma.eventTicketRange.findMany({
           where: { eventId: params.id },
@@ -74,44 +76,78 @@ export const getEventDashboardRoute = new Elysia()
             }
           }
         }),
+        prisma.eventTicketRange.findMany({
+          where: { eventId: params.id },
+          select: {
+            type: true,
+            _count: {
+              select: {
+                tickets: {
+                  where: {
+                    deliveredAt: { not: null },
+                    returned: false,
+                    memberId: { not: null },
+                  }
+                },
+              }
+            }
+          }
+        }),
+        prisma.eventTicketRange.findMany({
+          where: { eventId: params.id },
+          select: {
+            type: true,
+            _count: {
+              select: {
+                tickets: {
+                  where: {
+                    returned: true,
+                    deliveredAt: { not: null },
+                    memberId: { not: null },
+                  }
+                },
+              }
+            }
+          }
+        }),
       ]);
 
       const totalValue = totalValuePayedTickets.reduce((acc, ticket) => acc + ticket.amount, 0);
       const totalValueOnLastWeek = totalValuePayedTicketsOnLastWeek.reduce((acc, ticket) => acc + ticket.amount, 0);
 
-      const processedMembers = membersWithPizzaAndPaymentData.map((member) => {
-        const { calabresaCount, mistaCount } = member.tickets.reduce((acc, ticket) => {
-          if (ticket.number >= 0 && ticket.number <= 1000) {
-            acc.calabresaCount++;
-          } else if (ticket.number >= 2000 && ticket.number <= 3000) {
-            acc.mistaCount++;
-          }
-          return acc;
-        }, { calabresaCount: 0, mistaCount: 0 });
+      // const processedMembers = membersWithPizzaAndPaymentData.map((member) => {
+      //   const { calabresaCount, mistaCount } = member.tickets.reduce((acc, ticket) => {
+      //     if (ticket.number >= 0 && ticket.number <= 1000) {
+      //       acc.calabresaCount++;
+      //     } else if (ticket.number >= 2000 && ticket.number <= 3000) {
+      //       acc.mistaCount++;
+      //     }
+      //     return acc;
+      //   }, { calabresaCount: 0, mistaCount: 0 });
 
-        const totalPaymentsMade = member.payments.reduce((sum, payment) => sum + payment.amount, 0);
+      //   const totalPaymentsMade = member.payments.reduce((sum, payment) => sum + payment.amount, 0);
 
-        const totalPizzas = calabresaCount + mistaCount;
-        const totalPizzasCostExpected = totalPizzas * 50;
-        const isPaidOff = totalPaymentsMade >= totalPizzasCostExpected;
+      //   const totalPizzas = calabresaCount + mistaCount;
+      //   const totalPizzasCostExpected = totalPizzas * 50;
+      //   const isPaidOff = totalPaymentsMade >= totalPizzasCostExpected;
 
-        return {
-          memberId: member.id,
-          memberName: member.name,
-          calabresaPizzas: calabresaCount,
-          mistaPizzas: mistaCount,
-          totalPizzasOrdered: totalPizzas,
-          totalPaymentsMade,
-          totalPizzasCostExpected,
-          isPaidOff,
-          isAllConfirmedButNotYetFullyPaid: member.isAllConfirmedButNotYetFullyPaid,
-          status: isPaidOff ? 'Quitado' : 'Devendo',
-        };
-      });
+      //   return {
+      //     memberId: member.id,
+      //     memberName: member.name,
+      //     calabresaPizzas: calabresaCount,
+      //     mistaPizzas: mistaCount,
+      //     totalPizzasOrdered: totalPizzas,
+      //     totalPaymentsMade,
+      //     totalPizzasCostExpected,
+      //     isPaidOff,
+      //     isAllConfirmedButNotYetFullyPaid: member.isAllConfirmedButNotYetFullyPaid,
+      //     status: isPaidOff ? 'Quitado' : 'Devendo',
+      //   };
+      // });
 
-      const payedPerMember = processedMembers.filter((m) => m.isPaidOff).reduce((acc, member) => ({ calabresa: acc.calabresa + member.calabresaPizzas, mista: acc.mista + member.mistaPizzas }), { calabresa: 0, mista: 0 });
+      // const payedPerMember = processedMembers.filter((m) => m.isPaidOff).reduce((acc, member) => ({ calabresa: acc.calabresa + member.calabresaPizzas, mista: acc.mista + member.mistaPizzas }), { calabresa: 0, mista: 0 });
 
-      const possibleTotalTicketsData = processedMembers.filter((m) => m.isPaidOff || (!m.isPaidOff && m.isAllConfirmedButNotYetFullyPaid)).reduce((acc, member) => ({ totalTickets: acc.totalTickets + member.totalPizzasOrdered, calabresa: acc.calabresa + member.calabresaPizzas, mista: acc.mista + member.mistaPizzas }), { totalTickets: 0, calabresa: 0, mista: 0 });
+      // const possibleTotalTicketsData = processedMembers.filter((m) => m.isPaidOff || (!m.isPaidOff && m.isAllConfirmedButNotYetFullyPaid)).reduce((acc, member) => ({ totalTickets: acc.totalTickets + member.totalPizzasOrdered, calabresa: acc.calabresa + member.calabresaPizzas, mista: acc.mista + member.mistaPizzas }), { totalTickets: 0, calabresa: 0, mista: 0 });
 
       return {
         totalTickets,
@@ -121,23 +157,29 @@ export const getEventDashboardRoute = new Elysia()
         totalTicketsAfterImport,
         totalWithCritica,
         totalWithCriticaAndDelivered,
-        totalWithoutCriticaCalabresa,
-        totalWithoutCriticaMista,
         totalPayedTickets: Number((totalValue / 50).toFixed(0)),
         totalPayedTicketsOnLastWeek: Number((totalValueOnLastWeek / 50).toFixed(0)),
         totalValuePayedTickets: totalValue,
         totalValuePayedTicketsOnLastWeek: totalValueOnLastWeek,
-        totalCalabresaPayed: payedPerMember.calabresa,
-        totalMistaPayed: payedPerMember.mista,
-        possibleTotalTickets: possibleTotalTicketsData.totalTickets,
-        totalPredictedCalabresa: possibleTotalTicketsData.calabresa,
-        totalPredictedMista: possibleTotalTicketsData.mista,
+        // totalCalabresaPayed: payedPerMember.calabresa,
+        // totalMistaPayed: payedPerMember.mista,
+        // possibleTotalTickets: possibleTotalTicketsData.totalTickets,
+        // totalPredictedCalabresa: possibleTotalTicketsData.calabresa,
+        // totalPredictedMista: possibleTotalTicketsData.mista,
         totalMembers,
         totalTicketsPerRange: ticketEventRanges.map((range) => ({
           type: range.type,
           ticketCount: range._count.tickets,
         })),
         totalTicketsPerRangeLinkedToMembers: ticketEventRangesLinkedToMembers.map((range) => ({
+          type: range.type,
+          ticketCount: range._count.tickets,
+        })),
+        totalWithoutCriticaPerTicketEventRanges: totalWithoutCriticaPerTicketEventRanges.map((range) => ({
+          type: range.type,
+          ticketCount: range._count.tickets,
+        })),
+        totalWithCriticaPerTicketEventRanges: totalWithCriticaPerTicketEventRanges.map((range) => ({
           type: range.type,
           ticketCount: range._count.tickets,
         })),
@@ -161,23 +203,29 @@ export const getEventDashboardRoute = new Elysia()
           totalTicketsAfterImport: t.Number(),
           totalWithCritica: t.Number(),
           totalWithCriticaAndDelivered: t.Number(),
-          totalWithoutCriticaCalabresa: t.Number(),
-          totalWithoutCriticaMista: t.Number(),
           totalPayedTickets: t.Number(),
           totalPayedTicketsOnLastWeek: t.Number(),
           totalValuePayedTickets: t.Number(),
           totalValuePayedTicketsOnLastWeek: t.Number(),
-          totalCalabresaPayed: t.Number(),
-          totalMistaPayed: t.Number(),
-          possibleTotalTickets: t.Number(),
-          totalPredictedCalabresa: t.Number(),
-          totalPredictedMista: t.Number(),
+          // totalCalabresaPayed: t.Number(),
+          // totalMistaPayed: t.Number(),
+          // possibleTotalTickets: t.Number(),
+          // totalPredictedCalabresa: t.Number(),
+          // totalPredictedMista: t.Number(),
           totalMembers: t.Number(),
           totalTicketsPerRange: t.Array(t.Object({
             type: t.String(),
             ticketCount: t.Number(),
           })),
           totalTicketsPerRangeLinkedToMembers: t.Array(t.Object({
+            type: t.String(),
+            ticketCount: t.Number(),
+          })),
+          totalWithoutCriticaPerTicketEventRanges: t.Array(t.Object({
+            type: t.String(),
+            ticketCount: t.Number(),
+          })),
+          totalWithCriticaPerTicketEventRanges: t.Array(t.Object({
             type: t.String(),
             ticketCount: t.Number(),
           })),
